1c6be930a5cb45a1ec051bd23619a588b4496c6a
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-10-29 23:36:07 +0900

    課題1-1

---
+++ prmn2022a/src/lecture01/Main.java
@@ -0,0 +1,9 @@
+package lecture01;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ prmn2022a/src/lecture01/kadai_11.java
@@ -0,0 +1,9 @@
+package lecture01;
+
+public class kadai_11 {
+    public static void main(String[] args) {
+
+        System.out.println("B2212710 Sora Watanabe");
+
+    }
+}
