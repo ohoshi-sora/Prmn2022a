5a7e2233e0a88c93704ec0f52bd0771d071b7ebd
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-10-29 23:55:49 +0900

    課題1-2

---
+++ prmn2022a/src/lecture01/kadai_12.java
@@ -0,0 +1,12 @@
+package lecture01;
+
+public class kadai_12 {
+    public static void main(String[] args) {
+
+        int studentNumber = 2212710;
+        String studentName = " Sora Watanabe";
+
+        System.out.println("B" + studentNumber + studentName);
+
+    }
+}
