6b497c201cf0727668d689d1693a0998a94a64ca
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-02 13:50:35 +0900

    課題2-1

---
+++ prmn2022a/src/lecture02/kadai21.java
@@ -0,0 +1,12 @@
+package lecture02;
+
+public class kadai21 {
+    public static void main(String[] args) {
+
+        Human hum1 = new Human("たかし",18);
+        hum1.print();
+        Human hum2 = new Human("ひろし",20);
+        hum2.print();
+
+    }
+}
