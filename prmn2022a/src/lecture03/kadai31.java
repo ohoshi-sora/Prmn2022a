1ec1a47ee1f2d585748a03823877a549f1f0cb9b
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-06 21:13:41 +0900

    課題3-1

---
+++ prmn2022a/src/lecture03/kadai31.java
@@ -0,0 +1,15 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class kadai31 {
+    public static void main(String[] args) {
+
+        System.out.print("任意の文字列を入力してください：");
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        System.out.println("”" + input + "” と入力されました。");
+    }
+}
