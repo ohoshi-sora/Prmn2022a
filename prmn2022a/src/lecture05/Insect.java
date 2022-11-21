9b98dc1c67a4819cf967ebd67ac0773dfb0ca543
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-21 23:36:45 +0900

    課題5-1

---
+++ prmn2022a/src/lecture05/Insect.java
@@ -0,0 +1,9 @@
+package lecture05;
+
+public class Insect {
+
+    void move(){
+        System.out.println("歩いたよ");
+    }
+
+}
---
+++ prmn2022a/src/lecture05/Kadai51.java
@@ -0,0 +1,11 @@
+package lecture05;
+
+public class Kadai51 {
+    public static void main(String[] args) {
+
+        Insect insect = new Insect();
+
+        insect.move();
+
+    }
+}
