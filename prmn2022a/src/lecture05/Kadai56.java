853e6132756dec1be6cc77074a91fba2d0215cf0
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-22 00:50:42 +0900

    課題5-6

---
+++ prmn2022a/src/lecture05/Kadai56.java
@@ -0,0 +1,10 @@
+package lecture05;
+
+public class Kadai56 {
+    public static void main(String[] args) {
+
+        Insect insect = new SwallowtailButterfly();
+        insect.move();
+
+    }
+}
