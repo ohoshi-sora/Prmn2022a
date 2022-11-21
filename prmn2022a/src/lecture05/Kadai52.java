fee994971afe29b2def853d333f545d00215faad
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-22 00:37:31 +0900

    課題5-2

---
+++ prmn2022a/src/lecture05/Kadai52.java
@@ -0,0 +1,11 @@
+package lecture05;
+
+public class Kadai52 {
+    public static void main(String[] args) {
+
+        Butterfly butterfly = new Butterfly();
+
+        butterfly.move();
+
+    }
+}
