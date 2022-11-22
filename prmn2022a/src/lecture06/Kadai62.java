c2ba7e084c7eadd99989a6f275011fd1ea03e268
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-22 11:01:35 +0900

    課題6-2

---
+++ prmn2022a/src/lecture06/Kadai62.java
@@ -0,0 +1,29 @@
+package lecture06;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Kadai62 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>(6);
+
+        System.out.println("サイコロを5つ振りました.");
+        for (int i = 0; i < 5; i++) {
+            list.add(random.nextInt(6));
+        }
+        System.out.print("何番目のサイコロを確認しますか：");
+
+        try {
+            int x = input.nextInt();
+            System.out.println(list.get(x));
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ArrayListの範囲外アクセスを確認しました.");
+            System.out.println("プログラムを終了します.");
+        }
+
+    }
+}
