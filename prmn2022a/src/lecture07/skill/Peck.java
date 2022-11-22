6a0380b59bb9acde47ea6dcbc992ec4649616f65
ohoshi-sora <b2212710@photon.chitose.ac.jp>
2022-11-22 22:50:51 +0900

    課題7-1 skill

---
+++ prmn2022a/src/lecture07/skill/Peck.java
@@ -0,0 +1,4 @@
+package lecture07.skill;
+
+public class Peck {
+}
---
+++ prmn2022a/src/lecture07/skill/skill.java
@@ -0,0 +1,16 @@
+package lecture07.skill;
+
+public class skill  {
+
+    private String name;
+    private int power;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+}
